d92bd0f5d67ebfdbf7546b8dceb177372876fae2
Abhay <abhaykoli214@gmail.com>
2024-12-06 20:21:31 +0530

    changes

--- static/ludo/UI.js
+++ static/ludo/UI.js
@@ -1,91 +1,99 @@
-import { COORDINATES_MAP, PLAYERS, STEP_LENGTH } from './constants.js';
+import { COORDINATES_MAP, PLAYERS, STEP_LENGTH } from "./constants.js";
 
-const diceButtonElement = document.querySelector('#dice-btn');
+const diceButtonElement = document.querySelector("#dice-btn");
 const playerPiecesElements = {
-    P1: document.querySelectorAll('[player-id="P1"].player-piece'),
-    P2: document.querySelectorAll('[player-id="P2"].player-piece'),
-}
+  P1: document.querySelectorAll('[player-id="P1"].player-piece'),
+  P2: document.querySelectorAll('[player-id="P2"].player-piece'),
+};
 
 export class UI {
-    static listenDiceClick(callback) {
-        diceButtonElement.addEventListener('click', callback);
+  static listenDiceClick(callback) {
+    diceButtonElement.addEventListener("click", callback);
+  }
+
+  static listenResetClick(callback) {
+    document
+      .querySelector("button#reset-btn")
+      .addEventListener("click", callback);
+  }
+
+  static listenPieceClick(callback) {
+    document
+      .querySelector(".player-pieces")
+      .addEventListener("click", callback);
+  }
+
+  /**
+   *
+   * @param {string} player
+   * @param {Number} piece
+   * @param {Number} newPosition
+   */
+  static setPiecePosition(player, piece, newPosition) {
+    if (!playerPiecesElements[player] || !playerPiecesElements[player][piece]) {
+      console.error(
+        `Player element of given player: ${player} and piece: ${piece} not found`
+      );
+      return;
     }
 
-    static listenResetClick(callback) {
-        document.querySelector('button#reset-btn').addEventListener('click', callback)
+    const [x, y] = COORDINATES_MAP[newPosition];
+
+    const pieceElement = playerPiecesElements[player][piece];
+    pieceElement.style.top = y * STEP_LENGTH + "%";
+    pieceElement.style.left = x * STEP_LENGTH + "%";
+  }
+
+  static setTurn(index) {
+    if (index < 0 || index >= PLAYERS.length) {
+      console.error("index out of bound!");
+      return;
     }
 
-    static listenPieceClick(callback) {
-        document.querySelector('.player-pieces').addEventListener('click', callback)
+    const player = PLAYERS[index];
+
+    // Display player ID
+    document.querySelector(".active-player span").innerText = player;
+
+    const activePlayerBase = document.querySelector(".player-base.highlight");
+    if (activePlayerBase) {
+      activePlayerBase.classList.remove("highlight");
     }
+    // highlight
+    document
+      .querySelector(`[player-id="${player}"].player-base`)
+      .classList.add("highlight");
+  }
 
-    /**
-     * 
-     * @param {string} player 
-     * @param {Number} piece 
-     * @param {Number} newPosition 
-     */
-    static setPiecePosition(player, piece, newPosition) {
-        if(!playerPiecesElements[player] || !playerPiecesElements[player][piece]) {
-            console.error(`Player element of given player: ${player} and piece: ${piece} not found`)
-            return;
-        }
+  static enableDice() {
+    diceButtonElement.removeAttribute("disabled");
+  }
 
-        const [x, y] = COORDINATES_MAP[newPosition];
+  static disableDice() {
+    diceButtonElement.setAttribute("disabled", "");
+  }
 
-        const pieceElement = playerPiecesElements[player][piece];
-        pieceElement.style.top = y * STEP_LENGTH + '%';
-        pieceElement.style.left = x * STEP_LENGTH + '%';
-    }
+  /**
+   *
+   * @param {string} player
+   * @param {Number[]} pieces
+   */
+  static highlightPieces(player, pieces) {
+    pieces.forEach((piece) => {
+      const pieceElement = playerPiecesElements[player][piece];
+      pieceElement.classList.add("highlight");
+    });
+  }
 
-    static setTurn(index) {
-        if(index < 0 || index >= PLAYERS.length) {
-            console.error('index out of bound!');
-            return;
-        }
-        
-        const player = PLAYERS[index];
+  static unhighlightPieces() {
+    document.querySelectorAll(".player-piece.highlight").forEach((ele) => {
+      ele.classList.remove("highlight");
+    });
+  }
 
-        // Display player ID
-        document.querySelector('.active-player span').innerText = player;
-
-        const activePlayerBase = document.querySelector('.player-base.highlight');
-        if(activePlayerBase) {
-            activePlayerBase.classList.remove('highlight');
-        }
-        // highlight
-        document.querySelector(`[player-id="${player}"].player-base`).classList.add('highlight')
-    }
-
-    static enableDice() {
-        diceButtonElement.removeAttribute('disabled');
-    }
-
-    static disableDice() {
-        diceButtonElement.setAttribute('disabled', '');
-    }
-
-    /**
-     * 
-     * @param {string} player 
-     * @param {Number[]} pieces 
-     */
-    static highlightPieces(player, pieces) {
-        pieces.forEach(piece => {
-            const pieceElement = playerPiecesElements[player][piece];
-            pieceElement.classList.add('highlight');
-        })
-    }
-
-    static unhighlightPieces() {
-        document.querySelectorAll('.player-piece.highlight').forEach(ele => {
-            ele.classList.remove('highlight');
-        })
-    }
-
-    static setDiceValue(value) {
-        document.querySelector('.dice-value').innerText = value;
-    }
+  static setDiceValue(value) {
+    document.querySelector(".dice-value").innerText = value;
+  }
 }
 
 // UI.setPiecePosition('P1', 0, 0);
